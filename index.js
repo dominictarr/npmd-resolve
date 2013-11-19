2b024a597dd56f9f69854136cdf6b8dc7246dc3c
Dominic Tarr <dominic.tarr@gmail.com>
2013-11-20 01:43:06 +0800

    move npmd plugin stuff out

--- index.js
+++ index.js
@@ -1,56 +1,17 @@
-var path    = require('path')
-var ls       = require('npmd-tree').ls
+#! /usr/bin/env node
 
 var createResolvePackage = require('./resolve')
 var createResolve        = require('./resolve-tree')
 
-exports.db = function (db, config) {
-  var resolvePackage = createResolvePackage(db.sublevel('ver'), config)
+module.exports = function (db, config) {
+  return createResolve(createResolvePackage(db, config))
+}
+
+if(!module.parent) {
+  var resolvePackage = createResolvePackage(null, {})
   var resolve = createResolve(resolvePackage)
-  db.methods.resolve = {type: 'async'}
-  db.resolve = function (module, opts, cb) {
-    if(!cb) cb = opts, opts = {}
-    if(opts.hash) opts.greedy = false
-    resolve(
-      module,
-      opts,
-      cb
-    )
-  }
-}
-
-function merge (a, b) {
-  for(var k in b)
-    if(!a[k])
-      a[k] = b[k]
-  return a
-}
-
-exports.cli = function (db) {
-  db.commands.push(function (db, config, cb) {
-    var args = config._.slice()
-    var cmd = args.shift()
-    
-    if(cmd === 'resolve') {
-      if(!args.length)
-        args = deps(process.cwd(), config)
-
-      ls(function (err, tree) {
-        if(err) return cb(err)
-        db.resolve(args,
-          merge({
-            greedy: config.greedy,
-            available: tree
-          }, config),
-          function (err, tree) {
-            if(err) return cb(err)
-            console.log(JSON.stringify(tree, null, 2))
-            cb(null, tree)
-        })
-      })
-      return true
-    }
-
+  resolve(process.argv.slice(2), {}, function (err, tree) {
+    if(err) throw err
+    console.log(JSON.stringify(tree, null, 2))
   })
 }
-
