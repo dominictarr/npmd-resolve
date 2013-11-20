9335ceec3805e9482f322919fa902c8236146613
Dominic Tarr <dominic.tarr@gmail.com>
2013-11-20 13:01:41 +0800

    hash tarballs to get shasums!

--- offline.js
+++ offline.js
@@ -3,6 +3,7 @@ var fs = require('fs')
 var path = require('path')
 var tar = require('tar')
 var zlib = require('zlib')
+var crypto = require('crypto')
 
 function readJson(file, cb) {
   fs.readFile(file, 'utf-8', function (err, json) {
@@ -27,6 +28,35 @@ function untar(file, dir, cb) {
   .on('end', cb)
 }
 
+function sha(file, cb) {
+  var hash = crypto.createHash('sha1')
+  fs.createReadStream(file)
+  .on('data', function (d) { hash.update(d) })
+  .on('end', function () { cb(null, hash.digest('hex')) })
+  .on('error', cb)
+}
+
+function readJsonAndHash (cachedir, cb) {
+  var pkgfile =  path.join(cachedir,'package', 'package.json')
+  var pkgtarfile = path.join(cachedir, 'package.tgz')
+  var err, hash, pkg
+  sha(pkgtarfile, function (_err, _hash) { 
+    err =_err; hash = _hash; next()
+  })
+  readJson(pkgfile, function (_err, _pkg) {
+    err = _err; pkg = _pkg; next()
+  })
+
+  function next () {
+    if(err) return cb(err)
+    if(hash && pkg) {
+      pkg.shasum = hash
+      cb(err, pkg)
+    }
+  }
+
+}
+
 module.exports = function (module, vrange, opts, cb) {
 
   var cache = opts.cache || path.join(process.env.HOME, '.npm')
@@ -39,24 +69,22 @@ module.exports = function (module, vrange, opts, cb) {
     if(!ver)
       return cb(new Error('no module satified version:' + vrange))
       var cachedir = path.join(cache, module, ver)
-      var pkgfile =  path.join(cachedir,'package', 'package.json')
-      readJson(pkgfile, function (err, pkg) {
-        if(err && err.code == 'ENOENT') {
-          //maybe we have been installing things with npmd?
-          //if so, the package.json won't be unpacked.
-          //unpack it and try again.
-          return untar(
-            path.join(cachedir, 'package.tgz'),
-            cachedir,
-            function (err, pkg) {
-              if(err) return cb(err)
-              readJson(pkgfile, function (err, pkg) {
-                pkg.shasum = pkg.shasum || pkg.dist ? pkg.dist.shasum : undefined
-              })
-            })
-        }
-        cb(err, pkg)
-      })
+    readJsonAndHash(cachedir, function (err, pkg) {
+      if(err && err.code == 'ENOENT') {
+        //maybe we have been installing things with npmd?
+        //if so, the package.json won't be unpacked.
+        //unpack it and try again.
+        return untar(
+          pkgtarfile,
+          cachedir,
+          function (err, pkg) {
+            if(err) return cb(err)
+            readJsonAndHash(cachedir, cb)
+          })
+      }
+      cb(null, pkg)
+    })
+
   })
 }
 
