b1b224d236fa848ef465ae75b4c3770eaca6a056
Dominic Tarr <dominic.tarr@gmail.com>
2014-04-27 17:09:13 +0200

    rewrite offline resolution

--- offline.js
+++ offline.js
@@ -1,9 +1,7 @@
 var semver = require('semver')
 var fs = require('fs')
 var path = require('path')
-var tar = require('tar')
-var zlib = require('zlib')
-var crypto = require('crypto')
+var pull = require('pull-stream')
 
 function readJson(file, cb) {
   fs.readFile(file, 'utf-8', function (err, json) {
@@ -13,76 +11,49 @@ function readJson(file, cb) {
   })
 }
 
-function untar(file, dir, cb) {
-  fs.createReadStream(file)
-  .pipe(zlib.createGunzip())
-  .on('error', function (err) {
-    err.message = (
-        err.message
-      + '\n trying to unpack '
-      + file
-    )
-    cb(err)
-  })
-  .pipe(tar.Extract({path: dir}))
-  .on('end', cb)
-}
-
-function sha(file, cb) {
-  var hash = crypto.createHash('sha1')
-  fs.createReadStream(file)
-  .on('data', function (d) { hash.update(d) })
-  .on('end', function () { cb(null, hash.digest('hex')) })
-  .on('error', cb)
-}
-
-function readJsonAndHash (cachedir, cb) {
-  var err, hash, pkg
-  sha(path.join(cachedir, 'package.tgz'), function (_err, _hash) { 
-    err =_err; hash = _hash; next()
-  })
-  readJson(path.join(cachedir, 'package', 'package.json'), function (_err, _pkg) {
-    err = _err; pkg = _pkg; next()
-  })
-
-  function next () {
-    if(err) return cb(err)
-    if(hash && pkg) {
-      pkg.shasum = hash
-      cb(err, pkg)
-    }
-  }
-
-}
+var seen = {}
 
 module.exports = function (module, vrange, opts, cb) {
 
-  var cache = opts.cache || path.join(process.env.HOME, '.npm')
-  fs.readdir(path.join(cache, module), function (err, list) {
-    if(err) return cb(err)
-    list = list.filter(function (e) {
-      return semver.valid(e, true)
-    })
-    var ver = semver.maxSatisfying(list, vrange, true)
-    if(!ver)
-      return cb(new Error('no module satified version:' + vrange))
-      var cachedir = path.join(cache, module, ver)
-    readJsonAndHash(cachedir, function (err, pkg) {
-      if(err && err.code == 'ENOENT') {
-        //maybe we have been installing things with npmd?
-        //if so, the package.json won't be unpacked.
-        //unpack it and try again.
-        return untar(
-          path.join(cachedir, 'package.tgz'),
-          cachedir,
-          function (err, pkg) {
-            if(err) return cb(err)
-            readJsonAndHash(cachedir, cb)
-          })
-      }
-      cb(null, pkg)
-    })
+  if(!semver.validRange(vrange))
+    return cb()
 
+  var cache = opts.cache || path.join(process.env.HOME, '.npm')
+
+  readJson(path.join(cache, module, '.cache.json'), function (err, doc) {
+    if(err) return cb()
+
+    pull(
+      pull.values(Object.keys(doc.versions).sort(function (a, b) {
+        return semver.compareLoose(a, b) * -1
+      })),
+      pull.asyncMap(function (version, cb) {
+        var hash = doc.versions[version].dist.shasum
+
+//        var filename = path.join(opts.dbPath, 'blobs', hash.substring(0, 2), hash.substring(2))
+//        fs.stat(filename, function (err, stat) {
+//          if(err) cb()
+//          else    cb(null, doc.versions[version])
+//        })
+
+        var dirname = path.join(opts.dbPath, 'blobs', hash.substring(0, 2))
+        if(seen[hash]) return cb(null, doc.versions[version])
+        fs.readdir(dirname, function (err, ls) {
+          if(err) return cb()
+          ls.forEach(function (hash) {
+            seen[hash] = true
+          })
+          cb(null, seen[hash] ? doc.versions[version] : null)
+        })
+
+      }),
+      pull.filter(),
+      pull.take(1),
+      pull.collect(function (err, ary) {
+        if(err) return cb(err)
+        cb(null, ary[1])
+      })
+    )
   })
 }
 
